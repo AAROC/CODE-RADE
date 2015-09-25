f5473cc64c86bd2a11e9c16b594f8fbbcda00a6a
Bruce Becker <brucellino@gmail.com>
2015-09-25 18:01:33 +0200

    ADded the modules chekc

--- modules.sh
+++ modules.sh
@@ -6,8 +6,24 @@
 # If CVMFS is mounted at $CVMFS_MOUNT - usually /cvmfs then the modules are at
 # /cvmfs/<repo name>/modules
 
+# We need to set the following variables :
+
+# SITE - default = generic
+# OS - no default.
+# ARCH - default = x86_64
+
+SITE="generic"
+OS="undefined"
+ARCH="x86_64"
+CVMFS_MOUNT="undefined"
 
 # Is "modules even available? "
 if [ ! -n ${MODULESHOME} ] ; then
   echo "MODULESHOME is not set. Are you sure you have modules installed ? you're going to need it."
+  ecoh "Exiting"
+  exit 1;
 fi
+
+
+
+# Now figure out what $OS is.
