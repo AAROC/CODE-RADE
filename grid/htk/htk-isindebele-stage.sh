32c39b56e51c04eef34bb0f9d2c4e746aeb88dd6
Bruce Becker <brucellino@gmail.com>
2016-04-21 14:48:02 +0200

    Added example 2 - staging data

---
+++ grid/htk/htk-isindebele-stage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# CODE-RADE HTK Data Staging
+# Author : Bruce Becker
+#        : https://github.com/brucellino
+#        : CSIR Meraka
+# ########################################
+# See the README.md in the repo
+# This script runs a simple check to see if
+#  We can stage some HTK data from UJ
+# it is a standalone script to be used with exmaple 2
+# ########################################
+
+# Taking from the "system.sh" script in ASR, we first set up the workind dirs :
+export DIR_EXP=$PWD
+
+for dir in $DIR_EXP/data/mfccs $DIR_EXP/log $DIR_EXP/data/proc_trans $DIR_EXP/lists/; do
+  if [ ! -d $dir ]; then
+     mkdir -p $dir
+  fi
+done
+ls $DIR_EXP
+
+# Put the data into $DIR_EXP/data/audio
+# We will use just one chunk - this is passed as the argument
+echo "Staging chunnk $2"
+globus-url-copy gsiftp://fs01.grid.uj.ac.za/dpm/grid.uj.ac.za/home/sagrid/hlt-nwu/data/audio/isindebele_$2.tar.gz file:isindebele_$2.tar.gz
+ls -lht isindebele_$2.tar.gz
+tar xvfz isindebele_$2.tar.gz -C $DIR_EXP/data/audio/
+
+echo "size of the data is "
+du -chs $DIR_EXP/data/audio
+exit 0;
