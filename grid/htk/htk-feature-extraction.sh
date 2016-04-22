2cc02a99d124e46006e0a4de0465a082f35b07f1
Bruce Becker <brucellino@gmail.com>
2016-04-22 16:42:08 +0200

    Added the needed files to the input sandbox and added a line in the script to make cmn executable

--- grid/htk/htk-feature-extraction.sh
+++ grid/htk/htk-feature-extraction.sh
@@ -38,7 +38,7 @@ echo "creating hcopylist.lst"
 date >>  log/time.feat
 perl  create_hcopy_lists.pl  data/audio  data/mfccs  lists/hcopylist.lst
 echo "running: CMVN.sh cmvn"
-chmod +x CMVN.sh
+chmod +x CMVN.sh cvn.sh
 time ./CMVN.sh cmvn  lists/hcopylist.lst | tee  log/feature.log
 date >>  log/time.feat
 cat log/time.feat
