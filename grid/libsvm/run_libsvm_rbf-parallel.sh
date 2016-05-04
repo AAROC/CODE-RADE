fae998bef119bb15f8601a843bed2e927139c2e7
Bruce Becker <brucellino@gmail.com>
2016-05-04 14:07:04 +0200

    trying to find pngs

--- grid/libsvm/run_libsvm_rbf-parallel.sh
+++ grid/libsvm/run_libsvm_rbf-parallel.sh
@@ -433,8 +433,8 @@ do
       python $GRID_SEARCH/grid-parallel.py -log2c -13.2877,13.2877,1.6609 -log2g ${log},${log},0 -v 3 -m 300 $NGRAM_LINK/computation/train.data  > $NGRAM_LINK/result/result_${ngram}
 			python_end=$(date +%s.%N)
       python_time=$(echo "$python_end - $python_start" | bc)
-      ls -lht
-      file *
+      echo "Are there any pngs ?"
+      find . -name "*.png"
 
 		fi # ESTIMATE_C_VALUE
 
